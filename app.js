e8fbfc547b408c14e5e347ca38701bdc54be05f3
Andrew Pong <a@awp.io>
2016-04-04 15:20:28 -0700

    changing greeting

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var builder = require('botbuilder');
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
 bot.add('/', function (session) {
-  session.send('Hello World');
+  session.send('Hi World!');
 });
 
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 server.listen(process.env.port || 3978, function () {
-  console.log('%s listening to %s', server.name, server.url); 
+  console.log('%s listening to %s', server.name, server.url);
 });
