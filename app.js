2dc903eece7d47b97d343001e90e47c10d46692a
Andrew Pong <a@awp.io>
2016-04-04 15:49:04 -0700

    Updating code...

--- app.js
+++ app.js
@@ -36,5 +36,4 @@ server.post('/v1/messages',  piBot.listen());
 
 server.listen(8080, function () {
   console.log('%s listening to %s', server.name, server.url);
-  piBot.stdout('%s listening to %s', server.name, server.url);
 });
